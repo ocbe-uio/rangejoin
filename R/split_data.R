e41bb976ecda447c9d5cf44708c4e22a213bd919
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-01-23 12:11:28 +0100

    Changed indentation to 2 spaces

--- R/split_data.R
+++ R/split_data.R
@@ -3,36 +3,37 @@
 #' @param split_variable Binary variable to split the dataset
 #' @return A list with two data frames: case and ctrl
 split_data <- function(data, split_variable) {
-    if (missing(split_variable)) {
-        split_variable <- "group"
-        # Randomly assign cases to Control and Case
-        data[[split_variable]] <- sample(
-            c("Control", "Case"), nrow(data), replace = TRUE
-        )
-    }
-    data[[split_variable]] <- factor(data[[split_variable]])
-
-    # Validation
-    split_levels <- levels(factor(data[[split_variable]]))
-
-    ## Making sure split variable is binary
-    n_levels <- length(split_levels)
-    stopifnot("split_variable is not binary" = n_levels == 2)
-
-    ## Check if split variable is already {"Control", "Case"}
-    if (!all(split_levels %in% c("Control", "Case"))) {
-        # Transforming split variable into factor
-        data[[split_variable]] <- factor(
-            data[[split_variable]],
-            levels = c(0, 1),
-            labels = c("Control", "Case"),
-            ordered = FALSE
-        )
-    }
-
-    # Returning split data
-    list(
-        "case" = data[data[[split_variable]] == "Case", ],
-        "ctrl" = data[data[[split_variable]] == "Control", ]
+  if (missing(split_variable)) {
+    split_variable <- "group"
+    # Randomly assign cases to Control and Case
+    data[[split_variable]] <- sample(
+      c("Control", "Case"), nrow(data),
+      replace = TRUE
     )
+  }
+  data[[split_variable]] <- factor(data[[split_variable]])
+
+  # Validation
+  split_levels <- levels(factor(data[[split_variable]]))
+
+  ## Making sure split variable is binary
+  n_levels <- length(split_levels)
+  stopifnot("split_variable is not binary" = n_levels == 2)
+
+  ## Check if split variable is already {"Control", "Case"}
+  if (!all(split_levels %in% c("Control", "Case"))) {
+    # Transforming split variable into factor
+    data[[split_variable]] <- factor(
+      data[[split_variable]],
+      levels = c(0, 1),
+      labels = c("Control", "Case"),
+      ordered = FALSE
+    )
+  }
+
+  # Returning split data
+  list(
+    "case" = data[data[[split_variable]] == "Case", ],
+    "ctrl" = data[data[[split_variable]] == "Control", ]
+  )
 }
