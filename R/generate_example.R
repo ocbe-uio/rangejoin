c5596aa820806466aca0ec38970a8532c7fcc5a0
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-02-25 13:23:39 +0100

    Example data has twice more controls than cases (#1)

--- R/generate_example.R
+++ R/generate_example.R
@@ -1,7 +1,9 @@
 set.seed(1883312)
 n <- 100L
 example <- data.frame(
-  case_control = sample(c("Case", "Control"), n, replace = TRUE),
+  case_control = sample(
+    c("Case", "Control"), n, replace = TRUE, prob = c(1, 2)
+  ),
   age = rlnorm(n, meanlog = 3.8, sdlog = 0.15),
   sex = sample(c("Male", "Female"), n, replace = TRUE)
 )
