899dcf957a770b30b862f4b803751fe3623683d7
wleoncio <wleoncio@users.noreply.github.com>
2025-02-25 13:03:02 +0000

    Style code (GHA)

--- R/generate_example.R
+++ R/generate_example.R
@@ -2,7 +2,8 @@ set.seed(1883312)
 n <- 100L
 example <- data.frame(
   case_control = sample(
-    c("Case", "Control"), n, replace = TRUE, prob = c(1, 2)
+    c("Case", "Control"), n,
+    replace = TRUE, prob = c(1, 2)
   ),
   age = rlnorm(n, meanlog = 3.8, sdlog = 0.15),
   sex = sample(c("Male", "Female"), n, replace = TRUE)
