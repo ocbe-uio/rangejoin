3251d29dab1a48429f349a6c5b7e36ec96d613f7
Waldir Leoncio <w.l.netto@medisin.uio.no>
2025-04-03 11:31:40 +0200

    Added validation to data (#11)

--- R/range_join.R
+++ R/range_join.R
@@ -8,11 +8,19 @@
 #' @return A list with two data frames: control_data and case.
 #' @export
 range_join <- function(data, max_ctrl_per_case, age_range, quiet = TRUE) {
+  # Validating data structure
+  if (!is.list(data) || length(data) != 2) {
+    stop("
+      Data must be a list with two data frames: case and ctrl.
+      You may want to run split_data() to create a suitable list.
+    ")
+  }
+
   # Extracting data
   case <- data$case
   ctrl <- data$ctrl
 
-  # Validation
+  # Validating sizes of data frames
   stopifnot("Can't have more cases than controls" = nrow(ctrl) >= nrow(case))
   ctrl_per_case <- nrow(ctrl) / nrow(case)
   if (max_ctrl_per_case > ctrl_per_case) {
